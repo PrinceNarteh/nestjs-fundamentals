be5c0fa510825cebb1a6f16c6c309d20845c5476
Prince Narteh <prinarteh@gmail.com>
2024-05-10 21:25:42 -0700

    feat: add typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,20 @@
 import { Module } from '@nestjs/common';
 import { CoffeesModule } from './coffees/coffees.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [CoffeesModule],
+  imports: [
+    CoffeesModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      username: 'postgres',
+      password: 'admin',
+      host: 'localhost',
+      port: 5432,
+      database: 'nestjs_fundamentals',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
 })
-export class AppModule { }
+export class AppModule {}
